2578764665698eb99c8963443b9b1affc6e3ac03
Mohd Jaid <143423254+Mohd-Jaid@users.noreply.github.com>
2023-08-29 00:03:11 +0530

    Add files via upload
    My first Java program

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Hello {
+	public void men() {
+		System.out.println("They are two Mens");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello My World");
+		Hello call = new Hello();
+		
+		call.men();
+	}
+
+}
+  
